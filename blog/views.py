ff8946e6295ac088a327be71071595dead6211c2
yes5144 <1006793841@qq.com>
2018-09-24 23:41:35 +0800

    add csrf.js and ...

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import json
+from users.models import User
 
 # Create your views here.
 def index(request):
@@ -8,4 +10,10 @@ def test_jinja(request):
     return render(request, 'test_jinja.html')
 	
 def accounts_profile(request):
+    if request.method == 'POST':
+        a = json.loads(request.body)
+        print(a)
+        b = User.objects.get(email=request.user.email)
+        b.name = a['name']
+        b.save()
     return render(request, 'accounts_profile.html')
